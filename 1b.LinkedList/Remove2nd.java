b958d468f07b38719f5821a93536af8db4f19c16
sharanyar064 <rsharanya064@gmail.com>
2025-05-21 00:00:13 +0530

    Add files via upload

---
+++ 1b.LinkedList/Remove2nd.java
@@ -0,0 +1,22 @@
+// Write a java program for getting different colors through ArrayList interface and remove the 
+//2nd element and color "Blue" from the ArrayList
+
+package others;
+import java.util.*;
+public class Remove2nd {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			List<String> ls = new ArrayList<>();
+			ls.add("Orange");
+			ls.add("geen");
+			ls.add("pink");
+			ls.add("red");
+			ls.add("Blue");
+			System.out.println(ls);
+			ls.remove(2);
+			System.out.println(ls);
+			ls.remove("Blue");
+			System.out.println(ls);
+		}
+
+	}
