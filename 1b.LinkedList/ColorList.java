b6fd0f63818b710e4af9601aa3098045ce49133f
sharanyar064 <rsharanya064@gmail.com>
2025-05-22 00:03:16 +0530

    Update ColorList.java

--- 1b.LinkedList/ColorList.java
+++ 1b.LinkedList/ColorList.java
@@ -1,3 +1,7 @@
+// Write a java program for getting different colors through ArrayList interface and delete nth  
+// element from the ArrayList object  by  using remove by index
+
+
 package others;
 import java.util.ArrayList;
 import java.util.Scanner;
